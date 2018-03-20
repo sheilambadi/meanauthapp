e2368f2c64781631e7c32a8504d5dc5aa6565401
sheilambadi <mbadiatieno@gmail.com>
2018-03-20 20:20:57 +0300

    other user routes added

--- routes/users.js
+++ routes/users.js
@@ -8,4 +8,19 @@ router.get('/register', (req, res, next) => {
     res.send('Register');
 });
 
+//authenticate route
+router.get('/authenticate', (req, res, next) => {
+    res.send('Authenticate');
+});
+
+//profile route
+router.get('/profile', (req, res, next) => {
+    res.send('Profile');
+});
+
+//validate route
+router.get('/validate', (req, res, next) => {
+    res.send('Validate');
+});
+
 module.exports = router;
