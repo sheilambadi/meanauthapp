20484e738a0add69600956e7dcaa31b82c48625f
sheilambadi <mbadiatieno@gmail.com>
2018-03-22 10:28:06 +0300

    register user implemented

--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,29 @@
 
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
 
 //register route
 router.post('/register', (req, res, next) => {
-    res.send('Register');
+    //registering a new user
+    let newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.email,
+        password: req.body.passport
+    });
+
+    User.addUser(newUser, (err, user) => {
+        if(err){
+            res.json({success: false, msg: 'Failed to register user'});
+        } else {
+            res.json({success: true, msg: 'User registered'});
+            
+        }
+    });
 });
 
 //authenticate route
