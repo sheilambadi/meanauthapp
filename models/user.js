fc372bc0878377c374ed2a1f67e474d4bf189170
sheilambadi <mbadiatieno@gmail.com>
2018-03-22 10:34:47 +0300

    add user method added and password hashed

--- models/user.js
+++ models/user.js
@@ -32,3 +32,13 @@ module.exports.getUserByUsername = function(username, callback){
     const query = {username: username};
     User.findByOne(query, callback);
 }
+
+module.exports.addUser =function(newUser, callback){
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if(err) throw err;
+            newUser.password = hash;
+            newUser.save(callback);
+        });
+    });
+}
