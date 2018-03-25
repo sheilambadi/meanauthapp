bbf58176579915343783ca01e3423368d21fca17
sheilambadi <mbadiatieno@gmail.com>
2018-03-25 13:05:22 +0300

    add register ui

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  //properties
+  name: string;
+  username: string;
+  email: string;
+  password: string;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRegisterSubmit(){
+    //user object to be submitted
+    const user = {
+      name: this.name,
+      username: this.username,
+      email: this.email,
+      password: this.password
+    }
   }
 
 }
